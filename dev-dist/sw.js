f5ffe5865ec1b781c96e2239e28158fc9a5902e0
boburmirzo-brm <boburmirzo.brm@gmail.com>
2025-02-16 18:44:46 +0500

    fixed service worker errors

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.nuh547g55po"
+    "revision": "0.k0eu3mp9b88"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,8 @@
-import { useEffect, useState } from "react"
-
-interface Products {
-  id: number,
-  thumbnail: string,
-  title: string
-}
 
 const App = () => {
-  const [data, setData] = useState([])
-  useEffect(()=>{
-    const fetchData = async ()=>{
-      const response = await fetch("https://dummyjson.com/products")
-      response
-        .json()
-        .then(res => {
-          setData(res.products)
-        })
-    }
-    fetchData()
-  }, [])
-  
-  
   return (
     <div >
       <h2 className="text-center text-red-500">Car Wash</h2>
-      <div className="flex gap-3 flex-wrap">
-        {
-          data?.map((item:Products) => (
-            <div key={item.id}>
-              <img src={item.thumbnail} className="w-[300px]" alt="" />
-              <h3>{item.title}</h3>
-            </div>
-          ))
-        }
-      </div>
     </div>
   )
 }
