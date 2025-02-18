1179968052cda07f4a556d5fc90ebc529beef6f4
boburmirzo-brm <boburmirzo.brm@gmail.com>
2025-02-18 09:55:00 +0500

    fix some error releted shadcn

--- src/pages/auth/Auth.tsx
+++ src/pages/auth/Auth.tsx
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
 const Auth = () => {
-  const token = true// useSelector((state: RootState) => state.auth.access_token);
+  const token = useSelector((state: RootState) => state.auth.access_token);
   return token ? <Outlet /> : <Navigate replace to={"/login"} />;
 };
 
