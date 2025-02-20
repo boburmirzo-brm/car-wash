1395af137bd67a6fb331ad85d0e740306c08487b
boburmirzo-brm <boburmirzo.brm@gmail.com>
2025-02-20 11:38:42 +0500

    changed logo

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,15 @@
 import { Button } from "antd"
+import logo from "@/assets/images/pwa-192x192.png"
+import { Link } from "react-router-dom"
 
 const Header = () => {
   return (
     <header className="bg-slate-100 sticky top-0 left-0 ">
         <div className="container mx-auto">
             <nav className="h-[60px] max-[500px]:h-[50px] flex items-center justify-between">
-                <h2 className="text-2xl max-[500px]:text-xl font-medium text-primary select-none">Car Wash</h2>
+                <Link to={"/employer"} className="">
+                  <img className="w-[50px]" src={logo} alt="logo" />
+                </Link>
                 <Button>Profile</Button>
             </nav>
         </div>
