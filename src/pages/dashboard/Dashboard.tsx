75189c6d0c486e278343ac5c11d05f8e471fc815
boburmirzo-brm <boburmirzo.brm@gmail.com>
2025-02-20 11:02:10 +0500

    Added link

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom"
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div>
+      <h2>Dashboard</h2>
+      <Link to={"/employer"}>Employer</Link>
+    </div>
   )
 }
 
