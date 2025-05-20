70f70fbfb2866aa2b6a58515b6b25c6a894cbf8f
boburmirzo-brm <boburmirzo.brm@gmail.com>
2025-05-20 14:03:33 +0500

    Fix loading, bonus view, Statistic chart

--- src/pages/employee/expense/ExpenseHistory.tsx
+++ src/pages/employee/expense/ExpenseHistory.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useGetExpenseByEmployeeIdQuery } from "@/redux/api/expense";
 import Expense from "@/pages/expense/Expense";
-import { useLocation, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { RootState } from "@/redux";
 import useFilter from "@/hooks/useFilter";
@@ -11,9 +11,9 @@ const ExpenseHistory: React.FC = () => {
   const { id } = useParams<{ id?: string }>();
   const employeeId = useSelector((state: RootState) => state.auth.id);
   const userId = id || employeeId;
-  const { search } = useLocation();
-  const params = new URLSearchParams(search);
-  const filter = params.get("filter");
+  // const { search } = useLocation();
+  // const params = new URLSearchParams(search);
+  // const filter = params.get("filter");
 
   // useEffect(() => {
   //   if (filter === "admin") {
